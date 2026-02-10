62e319cec2f790210686d0ddb61c87bb3338381d
JashDolar09 <22amtics407@gmail.com>
2026-02-10 17:38:44 +0530

    4th next_Confif

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export', // Redirects are not supported with output: 'export'; handling at host/middleware level is required for static sites
+  output: 'export', 
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.parnikaindia.com',
-          },
-        ],
-        destination: 'https://parnikaindia.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/:path*',
+  //       has: [
+  //         {
+  //           type: 'host',
+  //           value: 'www.parnikaindia.com',
+  //         },
+  //       ],
+  //       destination: 'https://parnikaindia.com/:path*',
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
